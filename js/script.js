64864705de62a99028f5ae8792032a2fd249e0e9
Goraychenok <55753151+Goraychenok@users.noreply.github.com>
2020-10-30 00:26:02 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+      },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  jQuery(document).ready(function() {
+	$('.header__burger').click(function(event) {
+		$('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+        $('.header').addClass('active');
+    });
+});
+function slowScroll(id){
+	var offset = 10;
+	$('html,body').animate ({
+		scrollTop: $(id).offset ().top - offset
+	},1000);
+	$('body').removeClass('lock');
+	$('.header__burger,.header__menu').removeClass('active');
+	return false;
+}
+
